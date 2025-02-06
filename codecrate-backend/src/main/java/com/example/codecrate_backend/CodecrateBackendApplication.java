c3251999e93a73d2bfa3a391e17f223f630e4d09
Jaybert Bajenting <jaybertbajenting99@gmail.com>
2025-02-06 09:31:29 +0800

    Added Necessary Packages

--- codecrate-backend/src/main/java/com/example/codecrate_backend/CodecrateBackendApplication.java
+++ codecrate-backend/src/main/java/com/example/codecrate_backend/CodecrateBackendApplication.java
@@ -3,6 +3,11 @@ package com.example.codecrate_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
+
+
 @SpringBootApplication
 public class CodecrateBackendApplication {
 
