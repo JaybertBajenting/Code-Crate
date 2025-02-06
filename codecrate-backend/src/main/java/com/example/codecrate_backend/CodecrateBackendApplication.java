2cd0fa0b341a91a949dd8ef6940a4b302a9335a2
Jaybert Bajenting <jaybertbajenting99@gmail.com>
2025-02-06 09:25:53 +0800

    inital

---
+++ codecrate-backend/src/main/java/com/example/codecrate_backend/CodecrateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.codecrate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecrateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecrateBackendApplication.class, args);
+	}
+
+}
---
+++ codecrate-backend/src/test/java/com/example/codecrate_backend/CodecrateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecrate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecrateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
